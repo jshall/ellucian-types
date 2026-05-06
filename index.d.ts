592fccb5272c3af30987c3e821187d76c44d39c7
Josh Hall <josh.hall@indwes.edu>
2026-05-06 15:20:04 -0400

    added misc details

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 import { FunctionComponent } from "react";
 
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useDashboardInfo(): {
   version: string;
   erpType: "Banner" | "Colleague" | "PowerCampus" | "Quercus" | null;
@@ -22,12 +26,13 @@ export function useDashboardInfo(): {
     | "prodme";
 };
 
-export function useCardInfo<
-  CardConfiguration = {},
-  CustomConfiguration = {},
->(): {
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
+export function useCardInfo<Configuration = {}, CustomConfiguration = {}>(): {
   cardId: string;
-  configuration: CardConfiguration & {
+  configuration: Configuration & {
     customConfiguration: CustomConfiguration;
   };
   extensionId: string;
@@ -35,6 +40,7 @@ export function useCardInfo<
   externalLinkLabel: string;
 };
 
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html */
 export function useCardControl(): ReturnType<typeof useExtensionControl> & {
   drilldown(): void;
   resetDrilldown(): void;
@@ -46,6 +52,35 @@ export function useCardControl(): ReturnType<typeof useExtensionControl> & {
   setIsCustomConfigurationValid(): void;
 };
 
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html */
+export function usePageInfo(): {
+  /**
+   * The part of a page’s URL controlled by Experience. The format may change. Set your JavaScript router component to use this value as its base, so the router treats all URLs as relative to this base. For example, `<browserrouter basename={props.basePath}>`
+   */
+  basePath: string;
+};
+
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html */
+export function usePageControl(): {
+  /**
+   * A string passed to this function will be the title of the page. If this function is not specified, the page title matches the title of the card from which the page was launched.
+   * @param title Page title
+   */
+  setPageTitle(title: string): void;
+  setPageToolbar(): void;
+  setErrorMessage(): void;
+  setExitPrompt(): void;
+  setPageClosingCallback(): void;
+  setLoadingStatus(): void;
+  navigateToPage(): void;
+  getUrlToPage(): void;
+  closePage(): void;
+};
+
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useUserInfo(): {
   firstName: string;
   roles: string[];
@@ -55,6 +90,10 @@ export function useUserInfo(): {
   tenantAlias: string;
 };
 
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useThemeInfo(): {
   dashboardBackgroundColor: string;
   primaryColor: string;
@@ -67,6 +106,10 @@ export function useThemeInfo(): {
   };
 };
 
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useData(): {
   getEthosQuery<T>(params: {
     queryId: string;
@@ -80,6 +123,11 @@ export function useData(): {
   ): Promise<Response>;
 };
 
+/**
+ * All of these functions support the optional `scope` attribute. By default, stored items are scoped to the namespace of the card that created them. This ensures that values with the same key, in different cards, do not collide. You can use the `scope` attribute to define a custom scope that allows cards to share data with their corresponding pages or with other extension cards.
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useCache(): {
   getItem(params: {
     key: string;
@@ -100,6 +148,7 @@ export function useCache(): {
   clear(params: { scope: string; options: { session: boolean } }): void;
 };
 
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html */
 export function useExtensionInfo<TExtensionConfig = {}>(): {
   extensionId: string;
   configuration: TExtensionConfig;
@@ -107,6 +156,10 @@ export function useExtensionInfo<TExtensionConfig = {}>(): {
   type: "page" | "card";
 };
 
+/**
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html
+ * @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html
+ */
 export function useExtensionControl(): {
   setErrorMessage(options: {
     headerMessage: string;
@@ -117,17 +170,41 @@ export function useExtensionControl(): {
   setLoadingStatus(isLoading: boolean): void;
 };
 
+/** Undocumented */
+export function useExperienceInfo(): {}
+
+/** Undocumented */
+export function useTracker(): {
+  track: (e:unknown,t:unknown) => unknown
+}
+
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_card_props_sdk.html */
 export type ExperienceCard<
-  CardConfiguration = {},
+  Configuration = {},
   CustomConfiguration = {},
 > = FunctionComponent<{
   dashboardInfo: ReturnType<typeof useDashboardInfo>;
-  cardInfo: ReturnType<
-    typeof useCardInfo<CardConfiguration, CustomConfiguration>
-  >;
+  cardInfo: ReturnType<typeof useCardInfo<Configuration, CustomConfiguration>>;
   cardControl: ReturnType<typeof useCardControl>;
   userInfo: ReturnType<typeof useUserInfo>;
   themeInfo: ReturnType<typeof useThemeInfo>;
   data: ReturnType<typeof useData>;
   cache: ReturnType<typeof useCache>;
+}>;
+
+/** @link https://resources.elluciancloud.com/r/bundle/ellucian_experience/page/r_page_props_sdk.html */
+export type ExperiencePage<
+  Configuration = {},
+  CustomConfiguration = {},
+> = FunctionComponent<{
+  dashboardInfo: ReturnType<typeof useDashboardInfo>;
+  cardInfo: ReturnType<typeof useCardInfo<Configuration, CustomConfiguration>>;
+  extenstionInfo: ReturnType<typeof useExtensionInfo>;
+  pageInfo: ReturnType<typeof usePageInfo>;
+  pageControl: ReturnType<typeof usePageControl>;
+  userInfo: ReturnType<typeof useUserInfo>;
+  themeInfo: ReturnType<typeof useThemeInfo>;
+  data: ReturnType<typeof useData>;
+  cache: ReturnType<typeof useCache>;
+  tracker: ReturnType<typeof useTracker>;
 }>;
